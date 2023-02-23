3c214ab57f46f4fc930bcf408c475ffe9009aaaf
AlexisFaugeroux <faugeroux.alexis@gmail.com>
2023-02-23 13:39:37 +0100

    Initial CORS config

--- app/index.js
+++ app/index.js
@@ -1,21 +1,10 @@
 import express from 'express';
-// import cors from 'cors';
+import cors from 'cors';
 import router from './routers/index.js';
 
 const app = express();
 
-// app.use(cors({
-//     origin: 'http://localhost:3000',
-//     credentials: true,
-//     methods: 'GET,PUT,POST,OPTIONS',
-//     allowedHeaders: 'Content-Type',
-// }));
-
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Content-Type', 'application/json');
-//     next();
-// });
+app.use(cors());
 
 app.use(express.json());
 
--- app/routers/index.js
+++ app/routers/index.js
@@ -1,22 +1,12 @@
 import express from 'express';
 import apiController from '../controllers/apiController.js';
-import { corsWithOptions } from '../helpers/cors.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    console.log('Hello');
-    res.send('Hello');
-});
-
-router.route('/owner')
-    .options(corsWithOptions, (req, res) => { res.sendStatus(200); })
-    .post(corsWithOptions, apiController.getTokensByOwner);
+router.post('/owner', apiController.getTokensByOwner);
 
 router.post('/creator', apiController.getTokensByCreator);
 
-router.route('/candymachine')
-    .options(corsWithOptions, (req, res) => { res.sendStatus(200); })
-    .post(corsWithOptions, apiController.getCandyMachine);
+router.post('/candymachine', apiController.getCandyMachine);
 
 export default router;
