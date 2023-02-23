3eea2387b6abae661923628647c4a8185b8621c9
AlexisFaugeroux <faugeroux.alexis@gmail.com>
2023-02-23 15:13:54 +0100

    Initial CORS config

--- app/index.js
+++ app/index.js
@@ -1,24 +1,16 @@
 import express from 'express';
-// import cors from 'cors';
+import cors from 'cors';
 import router from './routers/index.js';
 
 const app = express();
 
-// app.use(cors());
+const corsOptions = {
+    origin: '*',
+};
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Request-Method', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Expose-Headers', 'Content-Type');
-
-    if (req.method === 'OPTIONS') {
-        res.writeHead(200);
-        return res.end();
-    }
-    return next();
-});
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
