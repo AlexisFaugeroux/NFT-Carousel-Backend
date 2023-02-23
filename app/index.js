4dbd4ee276a9d03d7907a117eaeefc442434c1e4
AlexisFaugeroux <faugeroux.alexis@gmail.com>
2023-02-23 11:19:53 +0100

    Middleware for CORS

--- app/index.js
+++ app/index.js
@@ -1,15 +1,20 @@
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 import router from './routers/index.js';
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-    methods: 'GET,PUT,POST,OPTIONS',
-    allowedHeaders: 'Content-Type,Authorization',
-}));
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+//     methods: 'GET,PUT,POST,OPTIONS',
+//     allowedHeaders: 'Content-Type',
+// }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 
 app.use(express.json());
 
