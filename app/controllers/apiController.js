da7b38ef19764357637776aef336587aa934cd81
AlexisFaugeroux <faugeroux.alexis@gmail.com>
2023-02-23 10:58:01 +0100

    Set CORS origin

--- app/controllers/apiController.js
+++ app/controllers/apiController.js
@@ -51,12 +51,6 @@ const apiController = {
             data: candyMachine,
         };
 
-        res.setHeader('Access-Control-Allow-Origin', '*');
-
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
-
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-
         return res.status(200).json(response);
     },
 };
--- app/index.js
+++ app/index.js
@@ -4,11 +4,14 @@ import router from './routers/index.js';
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: 'GET,PUT,POST,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+}));
+
 app.use(express.json());
-
-app.use(cors({ origin: true, credentials: true }));
-
-// app.options('*', cors());
 
 app.use(router);
 
