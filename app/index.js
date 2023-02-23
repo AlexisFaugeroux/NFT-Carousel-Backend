406d7289515ce8d50b4f9509a9838cbcd7f552e1
AlexisFaugeroux <faugeroux.alexis@gmail.com>
2023-02-23 12:48:27 +0100

    Add Content-type header

--- app/index.js
+++ app/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Header', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
